fe5b1507d058b8bfda83f4de632d48a54a9fa9e3
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-05-25 17:30:20 -0500

    yay rendering

---
+++ js/renderer.js
@@ -0,0 +1,32 @@
+class Renderer {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.camera = {
+      position: { x: 0, y: 0 },
+        zoom: 1e-9,
+        followRocket: true
+    };    this.planetImages = {};
+    this.imagesLoaded = false;
+    this.loadPlanetImages();
+  }
+
+  render(scene) {
+    // If no scene provided, render the space simulation
+    if (!scene) {
+      this.renderSpace();
+      return;
+    }
+
+    // Legacy scene-based rendering (if needed)
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    scene.objects.forEach(object => {
+      this.context.save();
+      this.context.translate(object.position.x, object.position.y);
+      this.context.rotate(object.rotation);
+      this.context.drawImage(object.image, -object.width / 2, -object.height / 2, object.width, object.height);
+      this.context.restore();
+    });
+  }  
+}
